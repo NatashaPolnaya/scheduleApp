8a82ee85840882e8b53042e393aa77a7b1a3d2a8
natashapolnaya <natashapolnaya@yandex.ru>
2021-03-11 12:24:03 +0300

    init project

--- scheduleApp/Controllers/HomeController.cs
+++ scheduleApp/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace scheduleApp.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
+        } 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
